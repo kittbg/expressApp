44d27213a492785463513f0f3145c594035b9144
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-04-07 11:08:25 -1000

    card build

--- public/script.js
+++ public/script.js
@@ -8,29 +8,29 @@ selectButton.on('click', function(){
 
 fetch('/api/music/albums')
   .then(response => response.json())
-  .then(albums => {
-      console.log(albums)
+  .then(data => {
+      console.log(data)
 
-      // for (let i = 0; i < data.length; i++){
-          //     let album = [];
-          //     let albums = data.
+      for (let i = 0; i < data.length; i++){
+          let artist = data[i];
           
           
+          let newDiv = $(`
+              <div class="card border-danger mb-3 bg-dark-subtle" style="max-width: 18rem;">
+                  <div class="card-body text-danger">
+                  <h1 class="card-header">Search Results</h1>
+                  <div class="band" class="card-text">Band: ${artist.album}</div>
+                  <div class="year" class="card-text">Year: ${artist.year}</div>
+                  <div class="songs" class="card-text">Songs: ${artist.songs}</div>
+                  <a tabindex="0" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Episode List" data-bs-content="${episodes.join('\n')}" aria-describedby="popover6159">Episodes</a>
+                  </div>
+                 </div> 
+                 `)
           
-          // let newDiv = $(`
-          //     <div class="card border-danger mb-3 bg-dark-subtle" style="max-width: 18rem;">
-          //         <div class="card-body text-danger">
-          //         <h1 class="card-header">Search Results</h1>
-          //         <div class="band" class="card-text">Band: ${result.species}</div>
-          //         <div class="year" class="card-text">Year: ${result.gender}</div>
-          //         <div class="songs" class="card-text">Songs: ${result.origin.name}</div>
-          //         <a tabindex="0" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Episode List" data-bs-content="${episodes.join('\n')}" aria-describedby="popover6159">Episodes</a>
-          //         </div>
-          //        </div> 
-          //        `)
-          
-          //        div.append(newDiv) 
+                 div.append(newDiv) 
+          }
         })
+
       })
 
    
